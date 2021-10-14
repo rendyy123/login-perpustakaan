d61dfad790e180631828f94665fbf65f3e2cbd42
rendyy123 <88363175+rendyy123@users.noreply.github.com>
2021-10-14 09:37:27 +0700

    Add files via upload

---
+++ pinjam_buku.php
@@ -0,0 +1,36 @@
+<?php 
+    include "header.php";
+    include "koneksi.php";
+    $qry_detail_buku=mysqli_query($conn,"select * from tabelbuku where id_buku = '".$_GET['id_buku']."'");
+    $dt_buku=mysqli_fetch_array($qry_detail_buku);
+?>
+<h2>Pinjam Buku</h2>
+<div class="row">
+    <div class="col-md-4">
+        <img src="indonesia.jpg" class="card-img-top">
+    </div>
+    <div class="col-md-8">
+        <form action="masukkankeranjang.php?id_buku=<?=$dt_buku['id_buku']?>" method="post">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <td>Nama Buku</td><td><?=$dt_buku['judul_buku']?></td>
+                    </tr>
+                    <tr>
+                        <td>penulis</td><td><?=$dt_buku['penulis']?></td>
+                    </tr>
+                    <tr>
+                        <td>Jumlah Pinjam</td><td><input type="number" name="jumlah_pinjam" value="1"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input class="btn btn-success" type="submit" value="PINJAM"></td>
+                    </tr>
+                </thead>
+            </table>
+        </form>
+    </div>
+</div>
+<?php 
+    include "footer.php";
+?>
+
